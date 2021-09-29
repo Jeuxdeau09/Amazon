4ac046f6f3adf6605c42a104793978b003bc33b3
Jeuxdeau <jeuxdeau2009@gmail.com>
2021-09-29 15:34:11 -0400

    added country list scroll down

--- src/screens/AddressScreen/index.tsx
+++ src/screens/AddressScreen/index.tsx
@@ -1,12 +1,29 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React ,{useState} from 'react';
+import {View, Text} from 'react-native';
+import {Picker} from '@react-native-picker/picker';
+import styles from './styles';
+import countryList from 'country-list';
+
+const countries = countryList.getData();
 
 const AddressScreen = () => {
-    return (
-        <View>
-           <Text> hELLOOO</Text>
-        </View>
-    );
+
+const [country, setCountry] =useState('');
+
+  return (
+    <View>
+      <View style={styles.row}>
+        <Picker
+        selectedValue = {country}
+        onValueChange = {setCountry}
+        >
+          {countries.map(country => (
+            <Picker.Item value={country.code} label={country.name} />
+          ))}
+        </Picker>
+      </View>
+    </View>
+  );
 };
 
 export default AddressScreen;
